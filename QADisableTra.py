00388cd104b4a83695d6e8473fe8e7dd769b5545
Syin-Lattice <40041311+Syin-Lattice@users.noreply.github.com>
2019-01-28 12:31:35 -0800

    Update QADisableTra.py

--- QADisableTra.py
+++ QADisableTra.py
@@ -81,12 +81,12 @@ def parser():
     if args.zk :
        DisableZK()
     else :
-       print ("Please select zk true , if you want to diable zk. Otherwise, zk won't be disbaled") 
+       print ("Please select zk true , if you want to disable zk. Otherwise, zk won't be disbaled") 
 
     if args.rds:
        DisableRDS()
     else: 
-       print ("Please select rds true , if you want to diable rds. Otherwise, rds won't be disbaled") 
+       print ("Please select rds true , if you want to disable rds. Otherwise, rds won't be disbaled") 
 
      
 if __name__ == '__main__':
